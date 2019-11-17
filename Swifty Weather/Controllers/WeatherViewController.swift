3a2f6ab268a464367b1039e8aeb1f6c629a45be9
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-11-17 16:18:38 +0200

    feat: added pageControl for locations view

--- Swifty Weather/Controllers/WeatherViewController.swift
+++ Swifty Weather/Controllers/WeatherViewController.swift
@@ -16,9 +16,14 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var pageControl: UIPageControl!
     
     //MARK: Variables
-    var weatherLocation: WeatherLocation!
+    let userDefault = UserDefaults.standard
+    
     var locationsManager: CLLocationManager?
     var currentLocation: CLLocationCoordinate2D!
+    
+    var allLocations: [WeatherLocation] = []
+    var allWeatherViews: [WeatherView] = []
+    var allWeatherData: [CityTempData] = []
     
     
     
@@ -27,51 +32,76 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         
         locationManagerStart()
+        weatherScrollView.delegate = self
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         
-       /* let weatherView = WeatherView()
-        weatherView.frame = CGRect(x: 0, y: 0, width: weatherScrollView.bounds.width, height: weatherScrollView.bounds.height)
-        weatherScrollView.addSubview(weatherView)
+        locationAuthCheck()
         
-        weatherLocation = WeatherLocation(city: "Cairo", country: "Egypt", countryCode: "EG", isCurrentLocation: false)
-        
-        getCurrentWeather(weatherView: weatherView)
-        getWeeklyWeather(weatherView: weatherView)
-        getHourlyWeather(weatherView: weatherView) */
-        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        locationManagerStop()
     }
     
     
     //MARK: Download Weather
     
     private func getWeather() {
-        
+        loadLocationsFromUserDefault()
+        createWeatherView()
+        addWeatherToScrollView()
+        setPageControlPageNumber()
     }
     
-    private func getCurrentWeather(weatherView: WeatherView) {
+    
+    private func createWeatherView() {
+        for _ in allLocations {
+            allWeatherViews.append(WeatherView())
+        }
+    }
+    
+    private func addWeatherToScrollView() {
+        for i in 0..<allWeatherViews.count {
+            let weatherView = allWeatherViews[i]
+            let location = allLocations[i]
+            
+            getCurrentWeather(weatherView: weatherView, location: location)
+            getHourlyWeather(weatherView: weatherView, location: location)
+            getWeeklyWeather(weatherView: weatherView, location: location)
+            
+            let xPosition = self.view.frame.width * CGFloat(i)
+            weatherView.frame = CGRect(x: xPosition, y: 0, width: weatherScrollView.bounds.width, height: weatherScrollView.bounds.height)
+            
+            weatherScrollView.addSubview(weatherView)
+            weatherScrollView.contentSize.width = weatherView.frame.width * CGFloat(i + 1)
+        }
+    }
+    
+    private func getCurrentWeather(weatherView: WeatherView, location: WeatherLocation) {
         
         weatherView.currentWeather = CurrentWeather()
-        weatherView.currentWeather.getCurrentWeather(location: weatherLocation) { (success) in
-            weatherView.refreshData()
+        weatherView.currentWeather.getCurrentWeather(location: location) { (success) in
+        weatherView.refreshData()
         }
         
     }
     
-    private func getWeeklyWeather(weatherView: WeatherView) {
+    private func getWeeklyWeather(weatherView: WeatherView, location: WeatherLocation) {
         
-        WeeklyWeatherForecast.downloadWeeklyWeatherForecast(location: weatherLocation) { (weatherForecast) in
+        WeeklyWeatherForecast.downloadWeeklyWeatherForecast(location: location) { (weatherForecast) in
             weatherView.weeklyWeatherForecastData = weatherForecast
             weatherView.tableView.reloadData()
         }
         
     }
     
-    private func getHourlyWeather(weatherView: WeatherView) {
+    private func getHourlyWeather(weatherView: WeatherView, location: WeatherLocation) {
         
-        HourlyForecast.downloadHourlyForecastWeather(location: weatherLocation) { (weatherForecast) in
+        HourlyForecast.downloadHourlyForecastWeather(location: location) { (weatherForecast) in
             weatherView.dailyWeatherForecastData = weatherForecast
             weatherView.hourlyCollectionView.reloadData()
         }
@@ -80,6 +110,31 @@ class WeatherViewController: UIViewController {
     
     
     
+    //MARK: Load users from userDefault
+    private func loadLocationsFromUserDefault() {
+        
+        let currentLocation = WeatherLocation(city: "", country: "", countryCode: "", isCurrentLocation: true)
+        
+        if let data = userDefault.value(forKey: "Locations") as? Data {
+            allLocations = try! PropertyListDecoder().decode(Array<WeatherLocation>.self, from: data)
+            allLocations.insert(currentLocation, at: 0)
+
+        } else {
+            print("no user data in user defaults")
+            allLocations.append(currentLocation)
+        }
+    }
+    
+    
+    //MARK: PageControl
+    private func setPageControlPageNumber() {
+        pageControl.numberOfPages = allWeatherViews.count
+        
+    }
+    
+    private func updatePageControlSelectedPage(currentPage: Int) {
+        pageControl.currentPage = currentPage
+    }
     
     //MARK: Location Manager
     private func locationManagerStart() {
@@ -128,5 +183,15 @@ extension WeatherViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get location, \(error.localizedDescription)")
+    }
+}
+
+
+
+extension WeatherViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let value = scrollView.contentOffset.x / scrollView.frame.size.width
+        updatePageControlSelectedPage(currentPage: Int(round(value)))
     }
 }
--- Swifty Weather/Extensions/RoundedView.swift
+++ Swifty Weather/Extensions/RoundedView.swift
@@ -11,11 +11,11 @@ import UIKit
 class RoundedView: UIView {
 
     override func awakeFromNib() {
-        self.layer.cornerRadius = 7
+        self.layer.cornerRadius = 12
 //        self.layer.shadowRadius = 3
 //        self.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
 //        self.layer.shadowOpacity = 0.3
-        self.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.5782052654)
     }
 
 }
